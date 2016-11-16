d5debfc6ca1357929890e6df260f8d1ea9c04c40
Jim Myhrberg <contact@jimeh.me>
2016-11-16 23:17:36 +0000

    Add proper command line flags and version support

--- main.go
+++ main.go
@@ -1,25 +1,37 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 
 	"github.com/jimeh/ozu.io/shortener"
 	"github.com/jimeh/ozu.io/storage/goleveldbstore"
 	"github.com/jimeh/ozu.io/web"
+	"gopkg.in/alecthomas/kingpin.v2"
 )
 
-func getPort() string {
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+// Version gets populated with version at build-time.
+var Version string
+var defaultPort = "8080"
 
-	return port
+var (
+	port = kingpin.Flag("port", "Port to listen to.").Short('p').
+		Default(defaultPort).String()
+	bind = kingpin.Flag("bind", "Bind address.").Short('b').
+		Default("0.0.0.0").String()
+	dir = kingpin.Flag("dir", "Directory to store database file.").
+		Short('d').Default("ozuio_database").String()
+	version = kingpin.Flag("version", "Print version info.").
+		Short('v').Bool()
+)
+
+func printVersion() {
+	fmt.Println("ozuio " + Version)
 }
 
-func main() {
-	store, err := goleveldbstore.New("ozuio_database")
+func startServer() {
+	store, err := goleveldbstore.New(*dir)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -28,5 +40,24 @@ func main() {
 	s := shortener.New(store)
 	server := web.NewServer(s)
 
-	log.Fatal(server.ListenAndServe(":" + getPort()))
+	if *port == defaultPort {
+		envPort := os.Getenv("PORT")
+		if envPort != "" {
+			*port = envPort
+		}
+	}
+
+	address := *bind + ":" + *port
+	fmt.Println("Listening on " + address)
+	log.Fatal(server.ListenAndServe(address))
+}
+
+func main() {
+	kingpin.Parse()
+
+	if *version {
+		printVersion()
+	} else {
+		startServer()
+	}
 }
